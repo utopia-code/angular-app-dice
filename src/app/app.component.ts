089f2538d6d0763a65c8de6d95778f715ecb5d51
utopia-code <utopiacode.developer@gmail.com>
2024-02-05 21:38:35 +0100

    added build application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dice';
+
+  diceLeft = `../assets/images/dice1.png`;;
+  diceRight = `../assets/images/dice4.png`;
+
+  constructor() { }
+
+  number1: number = 1;
+  number2: number = 4;
+
+  rollDice(): void {
+    this.number1 = Math.round(Math.random() * 5) + 1;
+    this.number2 = Math.round(Math.random() * 5) + 1;
+
+    this.diceLeft = `../assets/images/dice${this.number1}.png`;
+    this.diceRight = `../assets/images/dice${this.number2}.png`;
+  }
 }
